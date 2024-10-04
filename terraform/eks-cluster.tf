d12027181ba2b693ca8108e9deae04f9691a8ce1
VincentS <vincent.schultz.fr@gmail.com>
2024-10-04 10:37:50 +0200

    Modif config variables.tf terraform.tf & more comments

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,38 +1,57 @@
+# Déclaration d'un module Terraform pour créer un cluster EKS (Elastic Kubernetes Service) sur AWS
 module "eks" {
+  # Spécifie la source du module EKS. Ici, on utilise un module officiel Terraform disponible sur le registre
+  # terraform-aws-modules. La version utilisée est la 19.19.1.
   source  = "terraform-aws-modules/eks/aws"
   version = "19.19.1"
 
+  # Nom du cluster EKS à créer. Ce nom est défini dans une variable locale "local.cluster_name".
   cluster_name    = local.cluster_name
+
+  # Version du cluster Kubernetes (ici, 1.27) à déployer sur AWS.
   cluster_version = "1.27"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  # ID du VPC dans lequel le cluster EKS sera déployé. Ce VPC est défini par le module VPC.
+  vpc_id = module.vpc.vpc_id
+
+  # Liste des sous-réseaux privés où les nœuds du cluster seront déployés. Ceux-ci proviennent du module VPC.
+  subnet_ids = module.vpc.private_subnets
+
+  # Détermine si le point de terminaison API du cluster sera accessible publiquement. Ici, il est activé.
   cluster_endpoint_public_access = true
 
+  # Configuration par défaut pour les groupes de nœuds gérés par EKS.
   eks_managed_node_group_defaults = {
+    # Type d'AMI (Amazon Machine Image) utilisé par les nœuds. Ici, on utilise une AMI pour AL2 (Amazon Linux 2) avec architecture x86_64.
     ami_type = "AL2_x86_64"
-
   }
 
+  # Définition des groupes de nœuds gérés (Managed Node Groups) pour le cluster EKS.
   eks_managed_node_groups = {
+    # Premier groupe de nœuds (node group 1)
     one = {
+      # Nom du groupe de nœuds
       name = "node-group-1"
 
+      # Types d'instances EC2 utilisées par ce groupe de nœuds. Ici, des instances de type t3.small.
       instance_types = ["t3.small"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      # Configuration des tailles pour le scaling du groupe de nœuds
+      min_size     = 1  # Nombre minimum de nœuds
+      max_size     = 3  # Nombre maximum de nœuds
+      desired_size = 2  # Taille désirée du groupe au démarrage
     }
 
+    # Deuxième groupe de nœuds (node group 2)
     two = {
       name = "node-group-2"
 
+      # Type d'instance et taille de scaling pour ce groupe de nœuds (également des t3.small).
       instance_types = ["t3.small"]
 
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size     = 1  # Nombre minimum de nœuds
+      max_size     = 2  # Nombre maximum de nœuds
+      desired_size = 1  # Taille désirée du groupe au démarrage
     }
   }
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,26 +1,50 @@
 module "vpc" {
+  # Source du module VPC depuis le registre Terraform. Le module permet de créer une infrastructure réseau complète sur AWS.
   source  = "terraform-aws-modules/vpc/aws"
+  
+  # Version du module VPC utilisé.
   version = "5.1.2"
 
+  # Nom du VPC à créer. Ici, il est nommé "vprofile-eks" pour correspondre à un cluster EKS.
   name = "vprofile-eks"
 
+  # CIDR du VPC. Le bloc d'adresses IP attribué au réseau est 172.20.0.0/16.
   cidr = "172.20.0.0/16"
+  
+  # Zones de disponibilité (AZs) où le VPC sera déployé. Le code sélectionne les trois premières zones disponibles.
+  # data.aws_availability_zones.available.names provient d'une data source dans main.tf
+  # azs = [..., ..., ...] c'est une liste
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
+  # Sous-réseaux privés dans les AZs spécifiées. Ces sous-réseaux seront utilisés pour les ressources non accessibles publiquement.
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
+
+  # Sous-réseaux publics dans les mêmes AZs. Ces sous-réseaux sont utilisés pour les ressources accessibles publiquement comme les load balancers.
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
 
+  # Active une passerelle NAT (Network Address Translation) pour permettre aux instances dans les sous-réseaux privés d'accéder à Internet.
   enable_nat_gateway   = true
+
+  # Utilise une seule passerelle NAT au lieu d'une par zone de disponibilité, ce qui est plus économique.
   single_nat_gateway   = true
+
+  # Active les noms DNS pour les instances lancées dans le VPC, ce qui est utile pour les connexions avec des noms d'hôtes.
   enable_dns_hostnames = true
 
+  # Active le service de résolution DNS dans le VPC pour que les instances puissent résoudre des noms d'hôtes
+  #enable_dns_support   = true 
+
+  # Tags pour les sous-réseaux publics. 
+  # Le tag "kubernetes.io/cluster" indique que ces sous-réseaux sont partagés par un cluster Kubernetes.
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
+    "kubernetes.io/role/elb"                      = 1 # Tag pour indiquer qu'ils sont destinés aux ELB (Elastic Load Balancers)
   }
 
+  # Tags pour les sous-réseaux privés. 
+  # Le tag "kubernetes.io/role/internal-elb" indique que ces sous-réseaux sont destinés aux Load Balancers internes.
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
+    "kubernetes.io/role/internal-elb"             = 1 # Tag pour indiquer l'utilisation d'ELB internes
   }
 }
