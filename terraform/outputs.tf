3e217bb8ca3dfd7f8ba7c8463f13087853e95e42
VincentS <vincent.schultz.fr@gmail.com>
2024-10-05 04:33:58 +0200

    Add VPC id in output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,3 +18,8 @@ output "cluster_security_group_id" {
   description = "Security group ID for the Amazon Web Service EKS Cluster "
   value       = module.eks.cluster_security_group_id
 }
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
